dcd76ad0a92f46c65b7e6666e25452ac79575b69
Kczon-jelly <connell.kaden@yahoo.com>
2026-07-06 17:45:57 -0400

    Fix gitignore blocking migration files

---
+++ API/Migrations/Manga/20260706204913_AddMediaTypeAndLibraryRouting.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations.Manga
+{
+    /// <inheritdoc />
+    public partial class AddMediaTypeAndLibraryRouting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MediaType",
+                table: "Mangas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MediaType",
+                table: "MangaConnector",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DefaultForMediaType",
+                table: "FileLibraries",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MediaType",
+                table: "Mangas");
+
+            migrationBuilder.DropColumn(
+                name: "MediaType",
+                table: "MangaConnector");
+
+            migrationBuilder.DropColumn(
+                name: "DefaultForMediaType",
+                table: "FileLibraries");
+        }
+    }
+}
